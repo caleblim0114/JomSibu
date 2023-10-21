8a1350be78ed91b89ec22759200c2233b62f18ea
caleblim <limcaleb0114@gmail.com>
2023-10-21 16:31:23 +0800

    Create JomSibu MobileApp Project

---
+++ JomSibu/JomSibu/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JomSibu;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ JomSibu/JomSibu/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JomSibu;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
